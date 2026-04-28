dc0d751f29cf6ea5f01e2c3b5b565d91dba0e104
FelippeTM21 <felippetm21@gmail.com>
2026-04-28 17:43:39 -0400

    animation_code

---
+++ mateo_felippe_pablo_projet_vr/Assets/bouton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+  
+public class VRButton : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+}

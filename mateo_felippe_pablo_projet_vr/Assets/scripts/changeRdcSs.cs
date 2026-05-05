c025fac28dbca72af83ec21fcbf040cf5c044845
MRF <74323188+Cachton@users.noreply.github.com>
2026-05-05 14:17:10 -0400

    Door to main level v1
    The door and its script is done but not placed in the level yet. I need to add a specific spawnpoint for the player to appear at the right spot.

--- mateo_felippe_pablo_projet_vr/Assets/scripts/changeRdcSs.cs
+++ mateo_felippe_pablo_projet_vr/Assets/scripts/changeRdcSs.cs
@@ -3,16 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class DoorTouchTrigger : MonoBehaviour
 {
-    public DoorLock doorLock; // Reference to your lock script logic
+    public DoorLock doorLock; 
     public string sceneToLoad;
-    public GameObject loadingScreenUI; // Reference to your loading Canvas
+    public GameObject loadingScreenUI; 
 
     private void OnTriggerEnter(Collider other)
     {
-        // 1. Check if the door is actually unlocked
         if (doorLock != null && !doorLock.isLocked)
         {
-            // 2. Check if the object touching the door is a controller
+
             if (other.CompareTag("GameController")) 
             {
                 LoadNextLevel();
@@ -22,10 +21,8 @@ public class DoorTouchTrigger : MonoBehaviour
 
     private void LoadNextLevel()
     {
-        // Show the loading UI to the player
         if (loadingScreenUI != null) loadingScreenUI.SetActive(true);
 
-        // Load the scene
         SceneManager.LoadScene(sceneToLoad);
     }
 }
---
+++ mateo_felippe_pablo_projet_vr/Assets/scripts/changeSsRdc.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTouchTriggerSsRdc : MonoBehaviour
+{
+    public string sceneToLoad = "";
+    public string spawnPointToUse = "";
+    public GameObject loadingScreenUI; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.CompareTag("GameController")) 
+            {
+                LoadNextLevelSsRdc();
+            }
+    }
+
+    private void LoadNextLevelSsRdc()
+    {
+
+        if (loadingScreenUI != null) loadingScreenUI.SetActive(true);
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}

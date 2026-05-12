bdfd608b85a5496cf03bf7a29c45569b5a8b0ed2
MRF <74323188+Cachton@users.noreply.github.com>
2026-05-12 13:16:43 -0400

    Fade basement
    Fade is applied to the basement.

--- mateo_felippe_pablo_projet_vr/Assets/scripts/changeSsRdc.cs
+++ mateo_felippe_pablo_projet_vr/Assets/scripts/changeSsRdc.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,23 +7,27 @@ public class DoorTouchTriggerSsRdc : MonoBehaviour
 {
     public string sceneToLoad = "";
     public string spawnPointToUse = "";
-    public GameObject loadingScreenUI;
-
+    public Animator canvas;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("GameController"))
         {
-            LoadNextLevelSsRdc();
+            StartCoroutine("LoadLevel");
         }
     }
 
-    private void LoadNextLevelSsRdc()
+
+    IEnumerator LoadLevel()
     {
+        canvas.SetTrigger("start");
+        yield return new WaitForSeconds(2f);
+        LoadMain();
+        yield break;
+    }
 
-        if (loadingScreenUI != null) loadingScreenUI.SetActive(true);
-
+    public void LoadMain()
+    {
         SpawnManager.spawnPointName = spawnPointToUse;
-
-        SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene("main_scene");
     }
 }
